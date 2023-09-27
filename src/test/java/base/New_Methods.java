e3ff74531634203809865d000bb9f7eba9e48a81
rezwanul91 <mohammadhaquer@gmail.com>
2023-09-27 02:03:53 -0400

    line7 updated

--- src/test/java/base/New_Methods.java
+++ src/test/java/base/New_Methods.java
@@ -4,5 +4,6 @@ public class New_Methods {
     public static void main(String[] args) {
         System.out.println("I create this method for reuse the locators");
         System.out.println("I want to create another methods");
+        System.out.println("this is line number seven");
     }
 }
