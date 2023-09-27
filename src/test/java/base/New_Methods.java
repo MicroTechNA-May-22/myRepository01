8d4098db3878c59203f99b161474a6ffd03b1c8c
Mohammad Rezwanul Haque <136909905+rezwanul91@users.noreply.github.com>
2023-09-27 01:41:58 -0400

    Update New_Methods.java

--- src/test/java/base/New_Methods.java
+++ src/test/java/base/New_Methods.java
@@ -3,6 +3,7 @@ package base;
 public class New_Methods {
     public static void main(String[] args) {
         System.out.println("I create this method for reuse the locators");
+        System.out.println("i create another line");
        
     }
 }
