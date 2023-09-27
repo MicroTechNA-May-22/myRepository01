742765a86c6a0c74cf33f1a2ff26873e51e76989
Mohammad Rezwanul Haque <136909905+rezwanul91@users.noreply.github.com>
2023-09-27 01:37:39 -0400

    i delete a line in method class

--- src/test/java/base/New_Methods.java
+++ src/test/java/base/New_Methods.java
@@ -3,6 +3,6 @@ package base;
 public class New_Methods {
     public static void main(String[] args) {
         System.out.println("I create this method for reuse the locators");
-        System.out.println("i create new branch");
+       
     }
 }
