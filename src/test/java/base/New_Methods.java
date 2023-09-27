e23e436463bd9936ed190e468d431ab394d9883b
rezwanul91 <mohammadhaquer@gmail.com>
2023-09-27 01:44:40 -0400

    project updated

--- src/test/java/base/New_Methods.java
+++ src/test/java/base/New_Methods.java
@@ -3,7 +3,6 @@ package base;
 public class New_Methods {
     public static void main(String[] args) {
         System.out.println("I create this method for reuse the locators");
-        System.out.println("i create new branch");
-        System.out.println("i want to check merge conflict issue");
+        System.out.println("I want to create another methods");
     }
 }
