276b092b90fed55436c1980c03d364a3c2eed5d0
Ashit007 <137647912+Ashit007@users.noreply.github.com>
2023-09-29 13:30:10 -0400

    I am checking  conflict issue

--- src/test/java/base/AshitUpdate.java
+++ src/test/java/base/AshitUpdate.java
@@ -3,5 +3,6 @@ package base;
 public class AshitUpdate {
     public static void main (String[] args) {
         System.out.println ("I am trying in new Base class");
+          System.out.println ("conflict chekking");
     }
 }
