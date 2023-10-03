2a6978248e46f97537f14514e4fb1ec31491f840
Ashit <ashit.barua2023@gmail.com>
2023-10-02 22:20:58 -0400

    project update

--- src/test/java/base/AshitUpdate.java
+++ src/test/java/base/AshitUpdate.java
@@ -4,6 +4,7 @@ public class AshitUpdate {
     public static void main (String[] args) {
         System.out.println ("I am trying in new Base class");
         System.out.println ("I am cheking ***************************");
+        System.out.println ("I am pushing my project in jenkins");
 
     }
 }
