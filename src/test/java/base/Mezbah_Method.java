78ce15e3d1b6aff78b7a9ea8b17390c46aaa52ba
Mezbah17 <muahamed17@gmail.com>
2023-10-03 02:27:11 -0400

    Line Added

--- src/test/java/base/Mezbah_Method.java
+++ src/test/java/base/Mezbah_Method.java
@@ -3,5 +3,6 @@ package base;
 public class Mezbah_Method {
     public static void main(String[] args) {
         System.out.println("We love Belal Bhai");
+        System.out.println("I love Belal Bhai");
     }
 }
