3be3fb7d7af3b2ba5a1f661c76edf8c9e4d250d5
mduddin1924 <145969530+mduddin1924@users.noreply.github.com>
2024-02-27 01:46:47 -0500

    yummy yummy

--- src/test/java/base/Amena_base.java
+++ src/test/java/base/Amena_base.java
@@ -4,6 +4,7 @@ public class Amena_base {
     public static void main(String[] args) {
         System.out.println("we love mck" );
         System.out.println("after 5 thousand years make meal");
+          System.out.println("it was very tasty and yummy");
     }
 
 
