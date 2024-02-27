0598e36b81df160fe468149d4c6f0c921569c992
mduddin1924 <145969530+mduddin1924@users.noreply.github.com>
2024-02-27 01:45:44 -0500

    after 5000 years....meal

--- src/test/java/base/Amena_base.java
+++ src/test/java/base/Amena_base.java
@@ -3,6 +3,7 @@ package base;
 public class Amena_base {
     public static void main(String[] args) {
         System.out.println("we love mck" );
+        System.out.println("after 5 thousand years make meal");
     }
 
 
